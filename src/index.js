d17f4d2e83d2649e4869a02517075b29b60ba2e1
Nikita <ussnikita209@mail.ru>
2021-08-19 18:16:22 +0300

    feat: add + and - calculation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function eval() {
-    // Do not use eval!!!
-    return;
+function eval(a, b, operation) {
+    a = Number(a)
+    b = Number(b)
+    let res = 0
+    switch (operation) {
+        case '+': 
+            res = a + b
+            break
+        case '-':
+            res = a - b
+            break
+    }
+    return res;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/ /g, '')
+    exprArr = expr.split('')
+    let exprRes = 0
+    for (let i = 0, len = exprArr.length; i < len; i++) {
+        const curEl = exprArr[i]
+        if (curEl == '+' || curEl == '-') exprRes += eval(exprArr[i - 1],
+            exprArr[i + 1], curEl)
+    }
+    return exprRes
 }
 
 module.exports = {
