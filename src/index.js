fa7c158bd59cf5d49d1ff92145903ebd3462db1e
Nikita <ussnikita209@mail.ru>
2021-08-19 22:09:55 +0300

    feat: add multipy and div

--- src/index.js
+++ src/index.js
@@ -9,21 +9,56 @@ function eval(a, b, operation) {
         case '-':
             res = a - b
             break
+        case '*':
+            res = a * b
+            break
+        case '/':
+            res = a / b
+            break
     }
     return res;
 }
 
 function expressionCalculator(expr) {
+    const operations = ['*', '/', '+', '-']
     expr = expr.replace(/ /g, '')
     exprArr = expr.split('')
     let exprRes = 0
-    for (let i = 0, len = exprArr.length; i < len; i++) {
-        const curEl = exprArr[i]
-        if (curEl == '+' || curEl == '-') exprRes += eval(exprArr[i - 1],
-            exprArr[i + 1], curEl)
-    }
-    return exprRes
+    let i = 0
+    console.log(expr);
+    exprArr = calculateOper(exprArr, operations.slice(0, 2))
+    exprArr = calculateOper(exprArr, operations.slice(2))
+    console.log(exprArr);
+    return exprArr[0]
 }
+
+function calculateOper(arr, operations) {
+    let i = 0
+    let res = 0
+    while (i < arr.length) {
+        curEl = arr[i]
+
+        if (curEl == operations[0] || curEl == operations[1]) {
+            res = eval(arr[i - 1], arr[i + 1], curEl)
+
+            if (i + 1 < arr.length - 1) {
+                arr = arr.slice(0, i - 1).concat(res)
+                    .concat(
+                        arr.slice(i + 2)
+                    )
+            } else {
+                arr = arr.slice(0, i - 1).concat(res)
+                return arr
+            }
+            i--
+        }
+        i++
+
+    }
+    return arr
+}
+
+console.log(expressionCalculator('2*3'));
 
 module.exports = {
     expressionCalculator
